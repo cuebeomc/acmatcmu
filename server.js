7430a0ea2b566ff869de51a4bd61078d4f62ae1c
Cuebeom Choi <cuebeom.choi@gmail.com>
2019-08-28 18:27:33 -0400

    Added semicolons

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ var upload = multer({
 if (process.env.NODE_ENV == 'production') {
     app.use((req, res, next) => {
         if (req.header('x-forwarded-proto') !== 'https')
-            res.redirect(`https://${req.header('host')}${req.url}`)
+            res.redirect(`https://${req.header('host')}${req.url}`);
         else
-            next()
+            next();
     })
 }
 
