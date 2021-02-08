5185d0449519e7502c62b69f2d2c49eb6199aa05
Cuebeom Choi <cuebeom.choi@gmail.com>
2021-02-09 00:27:40 +0900

    Removed https and register

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-if (process.env.NODE_ENV == 'production') {
-    app.use((req, res, next) => {
-        if (req.header('x-forwarded-proto') !== 'https') {
-            res.redirect(`https://${req.header('host')}${req.url}`);
-        } else {
-            next();
-        }
-    });
-}
+// removed https
+// if (process.env.NODE_ENV == 'production') {
+//     app.use((req, res, next) => {
+//         if (req.header('x-forwarded-proto') !== 'https') {
+//             res.redirect(`https://${req.header('host')}${req.url}`);
+//         } else {
+//             next();
+//         }
+//     });
+// }
 
 /**
  *********************************
